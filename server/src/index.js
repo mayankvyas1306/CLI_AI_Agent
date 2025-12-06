780a7f984e8cdeb48359c4fe9a739671b759ea2c
mayankvyas1306 <vyas79303@gmail.com>
2025-12-06 23:22:09 +0530

    Initial commit: initialized client-shadcn and server-node,expressjs

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app=express();
+
+
+app.get("/health",(req,res)=>{
+    res.send("OK")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Your application is running on https://localhost:${process.env.PORT}`);
+})
